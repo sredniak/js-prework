2667dd900f4dd222714b3b502d23752a59370fba
Adrian Średniawski <sredni98123@gmail.com>
2023-08-10 12:39:33 +0200

    add player input

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ if (randomNumber == '1') {
     computerMove = 'nozyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nozyce';
+} else {
+    printMessage('Błędny wybór.')
+}
+printMessage('Twój ruch: ' + playerMove);
