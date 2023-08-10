9edc15a876d131476a29eb5ba5fb8c9fe9f5d8fe
Adrian Średniawski <sredni98123@gmail.com>
2023-08-10 12:24:26 +0200

    add else if in js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ printMessage(computerMove + 'Zagrałem ' + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage(playerMove + 'Zagrałem' + '! Jeśli Twój ruch to nozyczki, to wygrywasz');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if ( randomNumber == '2'){
+  computerMove = 'papier';
+} else if ( randomNumber == '3') {
+    computerMove = 'nozyce';
+}
+printMessage('Mój ruch: ' + computerMove);
